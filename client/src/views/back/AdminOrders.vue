737a977062dd0d7f2752523407f77bd541487ad8
Ilyes Kabrine <ilyeskabrine0@gmail.com>
2024-07-12 05:48:20 +0200

    after rebase bugs

--- client/src/views/back/AdminOrders.vue
+++ client/src/views/back/AdminOrders.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import { onMounted, ref } from 'vue';
 import DefaultLayout from '../../components/back/layouts/DefaultLayout.vue';
 import OrderCard from '@/components/back/componentsGeneric/order/ProductCard.vue';
 import ButtonDefault from '@/components/back/componentsGeneric/Buttons/ButtonDefault.vue';
