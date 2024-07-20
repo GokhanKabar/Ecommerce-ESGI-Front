a44a6754250e97f71866742b01db69278e5c4d13
MohaGons <mohagonssaib@gmail.com>
2024-07-20 16:54:16 +0200

    add refundement with stripe on order

--- client/src/views/back/ClientOrders.vue
+++ client/src/views/back/ClientOrders.vue
@@ -1,50 +1,42 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import DefaultLayout from '../../components/back/layouts/DefaultLayout.vue';
-import OrderCard from '../../components/back/componentsGeneric/order/ProductCard.vue';
-import ButtonDefault from '../../components/back/componentsGeneric/Buttons/ButtonDefault.vue';
-import BreadcrumbDefault from '../../components/back/componentsGeneric/Breadcrumbs/BreadcrumbDefault.vue';
-import OrderService from '../../services/OrderService';
-import store from '../../store/store.js';
+import { onMounted, ref } from 'vue'
+import DefaultLayout from '../../components/back/layouts/DefaultLayout.vue'
+import OrderCard from '../../components/back/componentsGeneric/order/ProductCard.vue'
+import ButtonDefault from '../../components/back/componentsGeneric/Buttons/ButtonDefault.vue'
+import BreadcrumbDefault from '../../components/back/componentsGeneric/Breadcrumbs/BreadcrumbDefault.vue'
+import OrderService from '../../services/OrderService'
+import store from '../../store/store.js'
 
-
-const pageTitle = ref('Mes Commandes');
-const Orders =ref([]);
-const user_id = store.state.user.id;
+const pageTitle = ref('Mes Commandes')
+const Orders = ref([])
+const user_id = store.state.user.id
 
 const fetchOrdersForUser = async () => {
   try {
-    const response = await OrderService.getOrderByUser(user_id);
-    Orders.value = response.formattedOrders;
+    const response = await OrderService.getOrderByUser(user_id)
+    Orders.value = response.formattedOrders
   } catch (error) {
-    console.error('Error fetching families:', error);
-    
+    console.error('Error fetching families:', error)
   }
-};
-
+}
 
 onMounted(async () => {
-  await fetchOrdersForUser();  
-  console.log('here'+Orders);
-});
-
+  await fetchOrdersForUser()
+  console.log('here' + Orders)
+})
 </script>
 <template>
-
-  
   <DefaultLayout>
-    <BreadcrumbDefault :pageTitle="pageTitle"  linkText="Accueil" linkTo="/"  />
+    <BreadcrumbDefault :pageTitle="pageTitle" linkText="Accueil" linkTo="/" />
     <div class="container mx-auto">
-<OrderCard :orders="Orders"/>
-
-
-      
+      <OrderCard :orders="Orders" />
     </div>
   </DefaultLayout>
 </template>
 
 <style scoped>
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
--- server/src/databases/sequelize/models/Order.js
+++ server/src/databases/sequelize/models/Order.js
@@ -1,88 +1,97 @@
-const { Model, DataTypes } = require('sequelize');
-
+const { Model, DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   class Order extends Model {
     static associate(models) {
-      
       Order.belongsToMany(models.Product, { through: models.ProductOrder });
       Order.belongsTo(models.User);
     }
   }
 
-  Order.init({
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+  Order.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      payment_intent_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [1, 50],
+        },
+      },
+      delivery_status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [1, 50],
+        },
+      },
+      payment_status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [1, 50],
+        },
+      },
+      total: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      date_order: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+          isDate: true,
+        },
+      },
+      date_creation: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+          isDate: true,
+        },
+      },
+      date_update: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        validate: {
+          isDate: true,
+        },
+      },
+      order_status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [1, 50],
+        },
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "User", // Assuming 'User' table exists for foreign key
+          key: "id",
+        },
+      },
     },
-    delivery_status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        len: [1, 50]
-      }
-    },
-    payment_status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        len: [1, 50]
-      }
-    },
-    total: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-      validate: {
-        notEmpty: true
-      }
-    },
-    date_order: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      validate: {
-        isDate: true
-      }
-    },
-    date_creation: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      validate: {
-        isDate: true
-      }
-    },
-    date_update: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      validate: {
-        isDate: true
-      }
-    },
-    order_status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        len: [1, 50]
-      }
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'User', // Assuming 'User' table exists for foreign key
-        key: 'id'
-      }
+    {
+      sequelize,
+      modelName: "Order",
+      timestamps: true,
+      underscored: true,
+      tableName: "Order",
     }
-  }, {
-    sequelize,
-    modelName: "Order",
-    timestamps: true,
-    underscored: true,
-    tableName: "Order",
-  });
+  );
 
   // Associations
 
