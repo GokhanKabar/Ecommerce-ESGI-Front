c5311ea70bc3d036d5205a830a9e8789a5f2cc40
GokhanKabar <gokhankabar@hotmail.fr>
2024-07-21 19:55:28 +0200

    fix import

--- server/src/routes/alert.route.js
+++ server/src/routes/alert.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const alertController = require('../controllers/alert.controller');
-const checkAuth = require('../middlewares/checkAuth');
+const checkAuth = require('../middlewares/checkAuthRole');
 
 router.get('/:userId', checkAuth({ checkUserId: true }), alertController.getAlerts);
 router.put('/:userId', checkAuth({ checkUserId: true }), alertController.updateAlerts);
